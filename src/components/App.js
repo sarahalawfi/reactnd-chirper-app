9942f185f7190f54de54e347af6c47a929c5f5d8
sara saleh alofi <saraalawfi1995@gmail.com>
2019-10-11 08:51:45 +0300

    Done all the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
+import Nav from './Nav'
 
 class App extends Component {
+  // to load intial data  from store to be use it in all other component
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
-      <div>
-        Starter Code
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className='container'>
+            <Nav />
+            {this.props.loading === true
+              ? null
+              : <div>
+                <Route path='/' exact component={Dashboard} />
+                <Route path='/tweet/:id' component={TweetPage} />
+                <Route path='/new' component={NewTweet} />
+              </div>}
+          </div>
+        </Fragment>
+      </Router>
     )
   }
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+
+// to connect the store that in provider to app we use it the connect()()
+//Using the connect() function upgrades a component to a container. 
+//Containers can read state from the store and dispatch actions.
+export default connect(mapStateToProps)(App)
